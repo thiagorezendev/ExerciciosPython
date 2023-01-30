06b0078274c47780b100eef98a3e6c6168bd9b3d
thiagorezendev <thiagor3zende@gmail.com>
2023-01-30 19:41:21 -0300

    exercícios feitos :zap:

--- ex034.py
+++ ex034.py
@@ -1,8 +1,6 @@
 sal = float(input('Escreva seu salário: '))
-if sal > 1.250:
-    salnovo = sal * 0.10
-    aumento = sal + salnovo
-else sal <= 1.250:
-    salnovo = sal * 0.15
-    aumento = sal + salnovo
+if sal <= 1250:
+    aumento = sal + (sal * 0.15)
+else:
+    aumento = sal + (sal * 0.10)
 print('Com o aumento, seu salário passa a ser R${:.2f}.'.format(aumento))
