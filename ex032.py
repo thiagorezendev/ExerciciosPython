20d2dc8300eb66ad1b5804f820b681e891ed17ea
thiagorezendev <thiagor3zende@gmail.com>
2023-01-30 11:13:10 -0300

    exercícios feitos :zap:

---
+++ ex032.py
@@ -0,0 +1,5 @@
+ano = int(input('Digite um ano para saber se ele é bissexto ou nao: '))
+if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+    print('O ano é bissexto!')
+else:
+    print('O ano não é bissexto!')
