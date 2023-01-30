3a4343bd6102cf685fe598f26942b35bb9b73536
thiagorezendev <thiagor3zende@gmail.com>
2023-01-30 18:58:20 -0300

    exercícios feitos :zap:

--- ex028.py
+++ ex028.py
@@ -1,8 +1,14 @@
 import random
-numPC = random.randint(1, 5)
-numHU = int(input('Digite um numero: '))
+from time import sleep
+numPC = random.randint(0, 5)
+print('---' * 20)
+print('Vou pensar em um número de 0 a 5, tente adivinha-lo...')
+print('---' * 20)
+numHU = int(input('Em que número pensei: '))
+print('Pensando...')
+sleep(1.5)
 if numHU == numPC:
     print('Parabens, voce acertou!')
 else:
     print('Errou!')
-print('O numero do PC era: {}'.format(numPC))
+print('O numero que eu pensei foi: {}'.format(numPC))
