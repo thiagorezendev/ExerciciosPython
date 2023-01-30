e4ef491a0920e0f391264e6c8b220afe35dcb1da
thiagorezendev <thiagor3zende@gmail.com>
2023-01-30 19:48:19 -0300

    exercícios feitos :zap:

--- ex035.py
+++ ex035.py
@@ -1,7 +1,7 @@
-r1 = int(input('Digite o primeiro comprimento: '))
-r2 = int(input('Digite o segundo comprimero: '))
-r3 = int(input('Digite o terceiro comprimento: '))
-if r1 + r2 < r3:
-    print('Pode formar um triângulo!')
+r1 = float(input('Digite o primeiro comprimento: '))
+r2 = float(input('Digite o segundo comprimero: '))
+r3 = float(input('Digite o terceiro comprimento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Podem formar um triângulo!')
 else:
-    print('Não pode formar um triângulo!')
+    print('Não podem formar um triângulo!')
