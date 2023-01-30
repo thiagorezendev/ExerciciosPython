7c56c6309cd50b535ef1fa432a931fd2271aa619
thiagorezendev <thiagor3zende@gmail.com>
2023-01-30 19:23:53 -0300

    exercícios feitos :zap:

--- ex031.py
+++ ex031.py
@@ -1,7 +1,8 @@
 dist = int(input('Digite a distância da viagem em km: '))
 if dist <= 200:
     preco = 0.50 * dist
-    print('Você irá pagar: R$ {:.2f}'.format(preco))
 else:
     preco = 0.45 * dist
-    print('Você irá pagar: R$ {:.2f}'.format(preco))
+print('Você irá pagar: R$ {:.2f}'.format(preco))
+
+# preco = dist * 0.50 if dist <= 200 else dist * 0.45
