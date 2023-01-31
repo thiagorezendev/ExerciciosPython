742192eb0600ca41795016bc4edd87e761e57e88
thiagorezendev <thiagor3zende@gmail.com>
2023-01-31 17:57:43 -0300

    exercícios feitos :zap:

---
+++ ex039.py
@@ -0,0 +1,12 @@
+from datetime import date
+nascimento = int(input('Informe seu ano de nascimento: '))
+ano = date.today().year
+idade = ano - nascimento
+if idade < 18:
+    tempo = 18 - idade
+    print('Ainda não chegou sua hora para alistamento militar! Faltam {} anos.'.format(tempo))
+elif idade == 18:
+    print('Está na hora de se alistar no exército!')
+else:
+    tempo = idade - 18
+    print('Passaram-se {} anos do tempo de alistamento, aliste-se já!'.format(tempo))
