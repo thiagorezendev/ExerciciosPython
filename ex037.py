b68cf8c6394e59e769376ba553f8084c9fa1f11c
thiagorezendev <thiagor3zende@gmail.com>
2023-01-31 17:36:50 -0300

    exercícios feitos :zap:

---
+++ ex037.py
@@ -0,0 +1,11 @@
+print('Escolha um número de acordo com o que deseja:')
+print('1 - Conversão para binario')
+print('2 - Conversão para octal')
+print('3 - Conversão para hexadecimal')
+num = int(input('Informe sua escolha: '))
+if num == 1:
+    print('Convertendo para binario...')
+elif  num == 2:
+    print('Convertendo para octal...')
+else:
+    print('Convertendo para hexadecimal...')
