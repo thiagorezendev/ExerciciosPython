13665bd6ac305bea9ab2ca5a0e72e15a3494be1c
thiagorezendev <thiagor3zende@gmail.com>
2023-01-31 18:08:29 -0300

    exercícios feitos :zap:

---
+++ ex041.py
@@ -0,0 +1,15 @@
+from datetime import date
+print(' - Informe o ano de nascimento para saber a categoria - ')
+nasc = int(input('Ano de nascimento: '))
+ano = date.today().year
+idade = ano - nasc
+if idade <= 9:
+    print('Categoria MIRIM.')
+elif idade <= 14:
+    print('Categoria JUVENIL.')
+elif idade <= 19:
+    print('Categoria JUNIOR.')
+elif idade <= 20:
+    print('Categoria SENIOR.')
+else:
+    print('Categoria MASTER.')
