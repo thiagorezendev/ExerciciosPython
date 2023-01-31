41eb322d7f7104930580ee2dcac110f946ad2838
thiagorezendev <thiagor3zende@gmail.com>
2023-01-31 17:31:10 -0300

    exercícios feitos :zap:

---
+++ ex036.py
@@ -0,0 +1,10 @@
+casa = float(input('Valor da casa: '))
+salario = float(input('Informe o seu salário: '))
+anos = float(input('Informe em quantos anos deseja pagar: '))
+prestacao = casa / (anos * 12)
+execeder = salario * 0.30
+if prestacao > execeder:
+    print('Empréstimo negado! Pois valor da prestação excede 30% do seu salário.')
+else:
+    print('Empréstimo aceito!')
+print('Valor da prestação: {:.2f}.'.format(prestacao))
