2ec1eb356d67b6a369513c2bcd1c61135349aea5
thiagorezendev <thiagor3zende@gmail.com>
2023-02-01 15:49:47 -0300

    Media Escolar :zap:

--- ex040.py
+++ ex040.py
@@ -1,6 +1,7 @@
 nota1 = float(input('Informe a primeira nota: '))
 nota2 = float(input('Informe a segunda nota: '))
 media = (nota1 + nota2) / 2
+print('Média de {:.2}'.format(media))
 if media < 5:
     print('Aluno reprovado!')
 elif media >= 5 and media < 6.9:
