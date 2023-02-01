b9fc3db4e88592daaba3658e02939bb1d7d86477
thiagorezendev <thiagor3zende@gmail.com>
2023-02-01 15:33:46 -0300

    Comparando números :zap:

--- ex038.py
+++ ex038.py
@@ -2,8 +2,8 @@ print('- Escreva 2 números para comparação -')
 num1 = int(input('Informe o primeiro número: '))
 num2 = int(input('Informe o segundo número: '))
 if num1 > num2:
-    print('O primeiro valor é maior.')
+    print('O primeiro valor é maior')
 elif num2 > num1:
-    print('O segundo valor é maior.')
+    print('O segundo valor é maior')
 else:
     print('Não existe valor maior, os dois são iguais.')
