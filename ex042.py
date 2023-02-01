1642befab73f16aa8d6957bfdbdc98e087883cdd
thiagorezendev <thiagor3zende@gmail.com>
2023-02-01 15:53:16 -0300

    Analisando Triângulos v2.0 :zap:

--- ex042.py
+++ ex042.py
@@ -1,6 +1,6 @@
-r1 = float(input('Digite o primeiro comprimento: '))
-r2 = float(input('Digite o segundo comprimero: '))
-r3 = float(input('Digite o terceiro comprimento: '))
+r1 = float(input('Digite o primeiro segmento: '))
+r2 = float(input('Digite o segundo segmento: '))
+r3 = float(input('Digite o terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Podem formar um triângulo ', end='')
     if r1 == r2 == r3:
