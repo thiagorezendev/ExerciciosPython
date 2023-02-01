d8fc527236846ad4f3c984381dd21aa313a8f70a
thiagorezendev <thiagor3zende@gmail.com>
2023-02-01 15:51:59 -0300

    Classificando Atletas :zap:

--- ex041.py
+++ ex041.py
@@ -3,13 +3,14 @@ print(' - Informe o ano de nascimento para saber a categoria - ')
 nasc = int(input('Ano de nascimento: '))
 ano = date.today().year
 idade = ano - nasc
+print('O atleta tem {} anos'.format(idade))
 if idade <= 9:
-    print('Categoria MIRIM.')
+    print('Categoria: MIRIM')
 elif idade <= 14:
-    print('Categoria JUVENIL.')
+    print('Categoria: JUVENIL')
 elif idade <= 19:
-    print('Categoria JUNIOR.')
+    print('Categoria: JUNIOR')
 elif idade <= 20:
-    print('Categoria SENIOR.')
+    print('Categoria: SENIOR')
 else:
-    print('Categoria MASTER.')
+    print('Categoria: MASTER')
