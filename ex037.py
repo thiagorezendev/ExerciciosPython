2f2c4d1b73a33620c82257550ef63c608504f47c
thiagorezendev <thiagor3zende@gmail.com>
2023-02-01 15:27:10 -0300

    Conversor de Bases Numéricas :zap:

--- ex037.py
+++ ex037.py
@@ -1,11 +1,14 @@
 print('Escolha um número de acordo com o que deseja:')
-print('1 - Conversão para binario')
+print('1 - Conversão para binário')
 print('2 - Conversão para octal')
 print('3 - Conversão para hexadecimal')
 num = int(input('Informe sua escolha: '))
+numero = int(input('Digite um numero: '))
 if num == 1:
-    print('Convertendo para binario...')
-elif  num == 2:
-    print('Convertendo para octal...')
+    print('{} em binário fica {}'.format(numero, bin(numero)[2:]))
+elif num == 2:
+    print('{} em octal fica {}'.format(numero, oct(numero)[2:]))
+elif num == 3:
+    print('{} em hexadecimal fica {}'.format(numero, hex(numero)[2:]))
 else:
-    print('Convertendo para hexadecimal...')
+    print('Opção inválida. Tente novamente.')
