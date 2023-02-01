8e3e768a24cdff352f020c0082f7551fce93dc7d
thiagorezendev <thiagor3zende@gmail.com>
2023-02-01 15:12:43 -0300

    Aprovando Empréstimo :zap:

--- ex036.py
+++ ex036.py
@@ -1,5 +1,5 @@
-casa = float(input('Valor da casa: '))
-salario = float(input('Informe o seu salário: '))
+casa = float(input('Valor da casa: R$'))
+salario = float(input('Informe o seu salário: R$'))
 anos = float(input('Informe em quantos anos deseja pagar: '))
 prestacao = casa / (anos * 12)
 execeder = salario * 0.30
