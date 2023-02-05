3e5e530e53da9006358f4edf3fb2da8b9d5fb502
thiagorezendev <thiagor3zende@gmail.com>
2023-02-05 18:23:49 -0300

    Tratando valores :zap:

--- ex064.py
+++ ex064.py
@@ -2,7 +2,7 @@ soma = 0
 cont = 0
 num = 0
 while num != 999:
-    num = int(input('Digite um número: '))
+    num = int(input('Digite um número (999 para parar): '))
     cont += 1
     soma = soma + num
 print('~' * 40)
