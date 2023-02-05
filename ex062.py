22dfb34f2110205eeac36d227c745fc99caced92
thiagorezendev <thiagor3zende@gmail.com>
2023-02-05 17:25:09 -0300

    Progressão Aritmética v2.0 :zap:

---
+++ ex062.py
@@ -0,0 +1,18 @@
+print('=' * 25)
+print('  10 TERMOS DE UMA P.A')
+print('=' * 25)
+termo = int(input('Primeiro termo: '))
+razao = int(input('Informe a razão: '))
+ter = termo
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} -> '.format(ter), end='')
+        ter += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Deseja mostrar mais quantos termos? '))
+print('Progressão finalizada com {} termos mostrado.'.format(total))
