a14179d3aa17cd8e757f779ed9d44ea82ecac066
thiagorezendev <thiagor3zende@gmail.com>
2023-02-07 16:09:44 -0300

    Vários números com flag :zap:

---
+++ ex066.py
@@ -0,0 +1,8 @@
+num = soma = cont = 0
+while True:
+    num = int(input('Digite um número: '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+print(f'Foram digitados {cont} números e a soma deles é {soma}')
