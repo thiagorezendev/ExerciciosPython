060fd4627d9b92880abd991c15166ea2dfa59c3c
thiagorezendev <thiagor3zende@gmail.com>
2023-02-10 17:06:23 -0300

    Contando vogais em Tupla :zap:

--- ex077.py
+++ ex077.py
@@ -1,7 +1,16 @@
+#palavras = ('neymar', 'ronaldinho', 'messi', 'zidade', 'cristiano',
+            #'mbappe', 'romario', 'cruyff', 'zlatan', 'neuer')
+#for i in palavras:
+    #print(f'\nNo nome {i.upper()} temos as vogais: ', end='')
+    #for letra in i:
+        #if letra.upper() in 'AEIOU':
+            #print(f'{letra}', end=' ')
+
+
 palavras = ('neymar', 'ronaldinho', 'messi', 'zidade', 'cristiano',
             'mbappe', 'romario', 'cruyff', 'zlatan', 'neuer')
-for i in palavras:
-    print(f'\nNo nome {i.upper()} temos as vogais: ', end='')
-    for letra in i:
+for p in palavras:
+    print(f'\nNo nome {p.upper()} temos as vogais', end='')
+    for letra in p:
         if letra.upper() in 'AEIOU':
             print(f'{letra}', end=' ')
