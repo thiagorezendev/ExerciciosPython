d30a0001fef9758db4d82c34e98a2aa5762f08af
thiagorezendev <thiagor3zende@gmail.com>
2023-02-23 17:44:22 -0300

    Um print especial :zap:

---
+++ ex097.py
@@ -0,0 +1,11 @@
+def escreva(txt):
+    tam = len(txt)
+    print('~' * tam)
+    print(txt)
+    print('~' * tam)
+
+
+escreva('Thiago Rezende')
+escreva('CLUBE ATLETICO MINEIRO')
+escreva('POlonia louca')
+escreva('abc')
