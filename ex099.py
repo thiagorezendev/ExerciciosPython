ccf2c8f2a7d50cbda6e27b3c649808c85a1a6906
thiagorezendev <thiagor3zende@gmail.com>
2023-02-24 11:35:49 -0300

    Função que descobre o maior :zap:

--- ex099.py
+++ ex099.py
@@ -1,5 +1,5 @@
 from time import sleep
-def maior(* num):
+def maior(* num): # Receber não sabe quantos parâmetros * representa todos(all)
     print('-=-' * 10)
     print('Analisando os valores passados...')
     tam = len(num)
@@ -7,12 +7,12 @@ def maior(* num):
     for n in num:
         print(f'{n} ', end='')
         sleep(0.5)
-        if c == 0:
+        if c == 0: # Na primeira posição sempre vai receber o primeiro número
             mai = n
         else:
             if n > mai:
                 mai = n
-        c += 1
+        c += 1 # Adiciona 1 a mais para verificar o maior número
     print(f'Foram informados {tam} valores ao todos.')
     print(f'O maior valor informado foi {mai}.')
 
