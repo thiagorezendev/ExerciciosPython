f53670985557b935d1514545abf44f9f3084e047
thiagorezendev <thiagor3zende@gmail.com>
2023-02-26 22:44:32 -0300

    Ficha do jogador :zap:

--- ex103.py
+++ ex103.py
@@ -8,8 +8,8 @@ def ficha(nome='<desconhecido>', gols=0):
 print('_' * 30)
 nome = str(input('Nome do jogador: '))
 gols = str(input('Número de gols: '))
-if gols.isnumeric():
+if gols.isnumeric(): # Verifico se é numérico porque string permite deixar o input sem ler nada
     gols = int(gols)
 else:
-    gols= 0
+    gols = 0
 ficha(nome, gols)
